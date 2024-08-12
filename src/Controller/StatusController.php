60a0e8b861c10266ce3a1c10db2da1db94a07398
Dinh Khanh Dang <dinhkhanhdang2k5@gmail.com>
2024-08-12 20:12:49 +0700

    update like button

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -1,19 +1,31 @@
 <?php
+// src/Controller/StatusController.php
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Entity\Status;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/status')]
 class StatusController extends AbstractController
 {
-    #[Route('/create', name: 'status_create')]
-    public function createStatus(Request $request, User $user): Response
-    {
-        
-    }
+#[Route('/like-status', name: 'like_status', methods: ['POST'])]
+public function likeStatus(Request $request, EntityManagerInterface $entityManager): JsonResponse
+{
+$statusId = $request->request->get('id');
+$status = $entityManager->getRepository(Status::class)->find($statusId);
+
+if (!$status) {
+return new JsonResponse(['error' => 'Status not found'], 404);
+}
+
+$likes = $status->getLikes();
+$status->setLikes($likes + 1);
+$entityManager->flush();
+
+return new JsonResponse(['likes' => $status->getLikes()]);
+}
 }
